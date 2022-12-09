f6597d0b198b2f35a824d1b9a9c2138f570839a7
Paul John <rainson.work@gmail.com>
2022-12-09 05:36:21 -0800

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints numbers
+ *
+ * Return: Always zero (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
