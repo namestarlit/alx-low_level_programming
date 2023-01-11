64759bba124880d155cfa56a68e9f0c2be564a95
Paul John <rainson.work@gmail.com>
2023-01-12 02:14:30 +0300

    a function that creates an array of chars, and initializes it with a specific char.

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,10 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
