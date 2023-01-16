bd5427909c54bc268e2d271f9d6fbfd29f32557b
Paul John <rainson.work@gmail.com>
2023-01-16 22:49:10 +0300

    added header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - structure for some dog
+ * @name: dog's name
+ * @owner: dog's owner name
+ * @age: dog's age
+ */
 struct dog
 {
 	char *name;
