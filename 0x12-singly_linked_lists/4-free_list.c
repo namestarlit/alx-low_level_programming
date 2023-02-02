126af17ebbdd33c0d4d133687280edb1757c1ee6
Paul John <rainson.work@gmail.com>
2023-02-02 17:01:34 +0300

    a function that frees a list_t list.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees (deletes) list_t list
+ * @head: head node
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	if (head != NULL)
+		ptr = head;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		free(ptr->str);
+		free(ptr);
+	}
+}
