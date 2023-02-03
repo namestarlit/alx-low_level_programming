b5ff126962aa4133e74bb8dab053a0e7e4fcb4f5
Paul John <rainson.work@gmail.com>
2023-02-03 15:45:08 +0300

    a function that frees list_t list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -13,7 +13,7 @@ void free_list(list_t *head)
 	while (current_node != NULL)
 	{
 		next_node = current_node->next;
-		free(current_node);
+		free(current_node->str);
 		free(current_node);
 		current_node = next_node;
 	}
