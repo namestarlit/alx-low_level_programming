4387a8f3e7434dda57c51ca74e42b3420cdba474
Paul John <rainson.work@gmail.com>
2023-02-03 18:06:33 +0300

    added comments

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,24 +12,30 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *tmp;
 
+	/* allocate memory for new node */
 	new_node = (listint_t *)malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
+	/* assign value to new node element n */
 	new_node->n = n;
+	/* assign new node pointer to point to NULL */
 	new_node->next = NULL;
 	tmp = *head;
 
+	/* assign new_node to head if it's the first node */
 	if (*head == NULL)
 	{
 		*head = new_node;
 	}
 	else
 	{
+		/* traverse list until you get to the last node */
 		while (tmp->next != NULL)
 		{
 			tmp = tmp->next;
 		}
+		/* assign pointer in tail node to point to new node */
 		tmp->next = new_node;
 	}
 	return (*head);
