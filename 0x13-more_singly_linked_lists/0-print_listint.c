d14de37fda63f77d201cc71c4b4b0321ba80a179
Paul John <rainson.work@gmail.com>
2023-02-03 18:13:03 +0300

    added comments

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,11 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
+	/* stores no. of nodes */
 	size_t no_nodes = 0;
 
+	/* traverse through all the elements of list */
 	while (h != NULL)
 	{
+		/* print element n */
 		printf("%d\n", h->n);
+		/* move to the next pointer */
 		h = h->next;
 		no_nodes++;
 	}
