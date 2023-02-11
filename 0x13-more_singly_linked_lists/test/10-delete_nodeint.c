5e0b4af7d7202d029c972b192d05970938ee84d3
Paul John <rainson.work@gmail.com>
2023-02-11 16:47:27 +0300

    modified

--- 0x13-more_singly_linked_lists/test/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/test/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		return (-1);
 	}
-	if (p_node->next && index != 0)
+	if (p_node->next == NULL && index != 0)
 	{
 		return (-1);
 	}
