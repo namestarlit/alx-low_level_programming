51a726fb11a0cf50a9c0d398aa733a337b09fb5f
Paul John <rainson.work@gmail.com>
2023-02-20 15:47:52 +0300

    test file

--- 0x13-more_singly_linked_lists/test/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/test/101-print_listint_safe.c
@@ -65,3 +65,88 @@ size_t print_listint_safe(const listint_t *head)
 	free_listp(&h_node);
 	return (no_nodes);
 }
+
+/**
+ * add_nodeint - adds new node at the beginning
+ * of a listint_t list
+ * @head: head node
+ * @n: integer (int)
+ *
+ * Return: head
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	/* allocate memory for the new node */
+	new_node = (listint_t *)malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	/* assign value of n to new_node element */
+	new_node->n = n;
+	/* point new node to head */
+	new_node->next = *head;
+	/* point head to new node */
+	*head = new_node;
+
+	return (*head);
+}
+
+/**
+ * print_listint - prints all elements of listint_t list
+ * @h: head node
+ *
+ * Return: no_nodes (no. of nodes)
+ */
+size_t print_listint(const listint_t *h)
+{
+	/* stores no. of nodes */
+	size_t no_nodes = 0;
+
+	/* traverse through all the elements of list */
+	while (h != NULL)
+	{
+		/* print element n */
+		printf("%d\n", h->n);
+		/* move to the next pointer */
+		h = h->next;
+		no_nodes++;
+	}
+	return (no_nodes);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+	listint_t *head2;
+	listint_t *node;
+
+	head2 = NULL;
+	add_nodeint(&head2, 0);
+	add_nodeint(&head2, 1);
+	add_nodeint(&head2, 2);
+	add_nodeint(&head2, 3);
+	add_nodeint(&head2, 4);
+	add_nodeint(&head2, 98);
+	add_nodeint(&head2, 402);
+	add_nodeint(&head2, 1024);
+	print_listint_safe(head2);
+	head = NULL;
+	node = add_nodeint(&head, 0);
+	add_nodeint(&head, 1);
+	add_nodeint(&head, 2);
+	add_nodeint(&head, 3);
+	add_nodeint(&head, 4);
+	node->next = add_nodeint(&head, 98);
+	add_nodeint(&head, 402);
+	add_nodeint(&head, 1024);
+	print_listint_safe(head);
+
+	return (0);
+}
