2d4205d5b1a3591c757661b44ebc165d1b99d32d
Paul John <rainson.work@gmail.com>
2023-02-27 06:47:59 +0300

    changed variable name

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,7 +10,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int set, max_bits;
+	unsigned long int s, max_bits;
 
 	/* get max number of bits of a number */
 	max_bits = sizeof(unsigned long int) * 8 - 1;
@@ -19,9 +19,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 
 	/* right shift 1 to index position(s) */
-	set = 1 << index;
+	s = 1 << index;
 	/* set the bit at given index pointed to by n to 1 */
-	*n = *n | set;
+	*n = *n | s;
 
 	return (1);
 }
