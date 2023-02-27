145b2c8a9c1fbe3f8c2a6200fa4facbc03af08b1
Paul John <rainson.work@gmail.com>
2023-02-27 08:54:12 +0300

    a function that checks the endianness.

---
+++ 0x14-bit_manipulation/test/100-get_endianness.c
@@ -0,0 +1,45 @@
+#include "../main.h"
+
+/**
+ * get_endianness - checks the endianness of the system
+ * (the order multi-byte data types are stored in mememory)
+ *
+ * Return: 0 (big endian), 1 (little endian)
+ */
+int get_endianness(void)
+{
+	int x = 1;
+	char *ptr;
+
+	/**
+	 * typecast int to char pointer.
+	 * if it's little endian system x (0x0001) will be stored
+	 * as (0x1000), the LSB 1 stored first.
+	 * if it's big endian system x will be stored
+	 * as (0x0001), the MSB 0 stored first.
+	 */
+	ptr = (char *)&x;
+
+	return (*ptr);
+}
+
+/**
+ * main - endianness of a system
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	n = get_endianness();
+	if (n != 0)
+	{
+		printf("Little Endian\n");
+	}
+	else
+	{
+		printf("Big Endian\n");
+	}
+	return (0);
+}
