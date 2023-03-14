9e72ffec1e8d6ac0ea5a2840a317987dba44e2a4
Paul John <rainson.work@gmail.com>
2023-03-14 16:01:43 +0300

    a program to read a text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the
+ * POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t lenr, lenw;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = (char *)malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	lenr = read(fd, buffer, leters);
+	close(fd);
+
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
+}
